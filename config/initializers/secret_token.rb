6a9aa226ebbc709e3ae99e8fd5ce7195dc56cbdb
MikePenhall <mikepenhall@gmail.com>
2013-06-29 12:26:11 +1200

    initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RiotRadioRails::Application.config.secret_key_base = 'ed4f96a50df42217d5ef72b14fd8a537dcb6c375b59b7dca44f2bb0250a4079188ed35948ab0546182780b509aa4afe9301c43e902f59a4ef5e46a4081a8a25f'
