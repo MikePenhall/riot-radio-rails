9c1fff768a614f4aa716b7d01d380732cd7ba171
MikePenhall <mikepenhall@gmail.com>
2013-06-29 14:21:13 +1200

    added travis.yml and basic database show template

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RiotRadioRails::Application.routes.draw do
   devise_for :administrators
+
+  authenticated :administrators do
+    root :to => 'dashboard#show', :as => :administrator_root
+  end
+
   root :to => 'dashboard#show'
 end
