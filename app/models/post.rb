f297b53ee68b81787371e18a86fbd014da91e91c
MikePenhall <mikepenhall@gmail.com>
2013-07-07 16:37:09 +1200

    added posts to resources, created slug generating programme

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,30 @@ class Post < ActiveRecord::Base
   validates :body, presence: :true
   validates :slug, { presence: :true, uniqueness: :true }
 
+  before_create :generate_slug
+
+
+  private
+    def generate_slug
+      slug = self.title.strip
+
+      #blow away apostrophes
+      slug.gsub! /['`]/,""
+
+      # @ --> at, and & --> and
+      slug.gsub! /\s*@\s*/, " at "
+      slug.gsub! /\s*&\s*/, " and "
+
+      #replace all non alphanumeric, underscore or periods with underscore
+      slug.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+
+      #convert double underscores to single
+      slug.gsub! /_+/,"_"
+
+      #strip off leading/trailing underscore
+      slug.gsub! /\A[_\.]+|[_\.]+\z/,""
+
+      self.slug = slug
+    end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 RiotRadioRails::Application.routes.draw do
-  resources :posts
 
   devise_for :administrators, path_prefix: "admin"
 
   resources :administrators
+  resources :posts
 
   root :to => 'dashboard#show'
 end
