d4a8753cfa84d267197d56ab9ab31ceeaf01f3fb
MikePenhall <mikepenhall@gmail.com>
2013-07-10 21:08:44 +1200

    tewaked posts show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,6 @@ class Post < ActiveRecord::Base
   before_validation :check_for_slug
 
   private
-    def format_tags_and_categories
-      self.tags.split(",")
-      self.categories.split(",")
-    end  
 
     def check_for_slug
       if !self.slug.present?
@@ -27,6 +23,5 @@ class Post < ActiveRecord::Base
         string = self.title
       end  
       self.slug = string.parameterize.dasherize.downcase
-    end  
-
+    end     
 end
