3edb953b509b668da0b4aa5c7a8140ef658ca612
MikePenhall <mikepenhall@gmail.com>
2013-07-11 18:24:19 +1200

    reduced the number of failing post controller specs

--- spec/controllers/admin/posts_controller_spec.rb
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -18,22 +18,22 @@ require 'spec_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-describe PostsController do
+describe Admin::PostsController do
 
   # This should return the minimal set of attributes required to create a valid
   # Post. As you add validations to Post, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "title" => "MyString" } }
+  let(:post) { create(:post) }
+  let(:valid_attributes) { attributes_for(:post) }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # PostsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
 
   describe "GET index" do
     it "assigns all posts as @posts" do
       post = Post.create! valid_attributes
-      get :index, {}, valid_session
+      get :index
       assigns(:posts).should eq([post])
     end
   end
@@ -41,14 +41,14 @@ describe PostsController do
   describe "GET show" do
     it "assigns the requested post as @post" do
       post = Post.create! valid_attributes
-      get :show, {:id => post.to_param}, valid_session
+      get :show, {:id => post.to_param}
       assigns(:post).should eq(post)
     end
   end
 
   describe "GET new" do
     it "assigns a new post as @post" do
-      get :new, {}, valid_session
+      get :new
       assigns(:post).should be_a_new(Post)
     end
   end
@@ -56,7 +56,7 @@ describe PostsController do
   describe "GET edit" do
     it "assigns the requested post as @post" do
       post = Post.create! valid_attributes
-      get :edit, {:id => post.to_param}, valid_session
+      get :edit, {:id => post.to_param}
       assigns(:post).should eq(post)
     end
   end
@@ -65,18 +65,18 @@ describe PostsController do
     describe "with valid params" do
       it "creates a new Post" do
         expect {
-          post :create, {:post => valid_attributes}, valid_session
+          post :create, {:post => valid_attributes}
         }.to change(Post, :count).by(1)
       end
 
       it "assigns a newly created post as @post" do
-        post :create, {:post => valid_attributes}, valid_session
+        post :create, {:post => valid_attributes}
         assigns(:post).should be_a(Post)
         assigns(:post).should be_persisted
       end
 
       it "redirects to the created post" do
-        post :create, {:post => valid_attributes}, valid_session
+        post :create, {:post => valid_attributes}
         response.should redirect_to(Post.last)
       end
     end
@@ -85,14 +85,14 @@ describe PostsController do
       it "assigns a newly created but unsaved post as @post" do
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
-        post :create, {:post => { "title" => "invalid value" }}, valid_session
+        post :create, {:post => { :title => "" }}
         assigns(:post).should be_a_new(Post)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
-        post :create, {:post => { "title" => "invalid value" }}, valid_session
+        post :create, {:post => { :title => "" }}
         response.should render_template("new")
       end
     end
@@ -107,19 +107,19 @@ describe PostsController do
         # receives the :update_attributes message with whatever params are
         # submitted in the request.
         Post.any_instance.should_receive(:update).with({ "title" => "MyString" })
-        put :update, {:id => post.to_param, :post => { "title" => "MyString" }}, valid_session
+        put :update, {:id => post.to_param, :post => { "title" => "MyString" }}
       end
 
       it "assigns the requested post as @post" do
         post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => valid_attributes}, valid_session
+        put :update, {:id => post.to_param, :post => valid_attributes}
         assigns(:post).should eq(post)
       end
 
       it "redirects to the post" do
         post = Post.create! valid_attributes
-        put :update, {:id => post.to_param, :post => valid_attributes}, valid_session
-        response.should redirect_to(post)
+        put :update, {:id => post.to_param, :post => valid_attributes}
+        response.should redirect_to(admin_post_url)
       end
     end
 
@@ -128,7 +128,7 @@ describe PostsController do
         post = Post.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
-        put :update, {:id => post.to_param, :post => { "title" => "invalid value" }}, valid_session
+        put :update, {:id => post.to_param, :post => { "title" => "" }}
         assigns(:post).should eq(post)
       end
 
@@ -136,7 +136,7 @@ describe PostsController do
         post = Post.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Post.any_instance.stub(:save).and_return(false)
-        put :update, {:id => post.to_param, :post => { "title" => "invalid value" }}, valid_session
+        put :update, {:id => post.to_param, :post => { "title" => "" }}
         response.should render_template("edit")
       end
     end
@@ -146,14 +146,14 @@ describe PostsController do
     it "destroys the requested post" do
       post = Post.create! valid_attributes
       expect {
-        delete :destroy, {:id => post.to_param}, valid_session
+        delete :destroy, {:id => post.to_param}
       }.to change(Post, :count).by(-1)
     end
 
     it "redirects to the posts list" do
       post = Post.create! valid_attributes
-      delete :destroy, {:id => post.to_param}, valid_session
-      response.should redirect_to(posts_url)
+      delete :destroy, {:id => post.to_param}
+      response.should redirect_to(admin_posts_url)
     end
   end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,5 +8,5 @@ describe Post do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
   it { should validate_uniqueness_of :slug }
-
+  
 end
